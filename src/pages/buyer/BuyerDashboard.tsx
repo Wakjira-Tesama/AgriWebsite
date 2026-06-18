13efb54ab12371c17c3cc4fcbd983962e0854b16
Wakjira-Tesama <wakjiratesama@gmail.com>
2026-06-19 00:45:00 +0300

    feat(buyer): create marketplace items array

--- src/pages/buyer/BuyerDashboard.tsx
+++ src/pages/buyer/BuyerDashboard.tsx
@@ -201,4 +201,10 @@ export default function BuyerDashboard() {
             </p>
           </div>
         ))}
-      </div>
+      </div>
+
+      {/* Charts Row */}
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+        {/* Spending Chart */}
+        <div className="lg:col-span-2 bg-card rounded-2xl border border-border p-6">
+          <div className="flex justify-between items-center mb-6">
