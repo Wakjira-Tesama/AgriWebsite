2b9ab98e1c69a3b19c50a34b5437a252e05c49fd
Wakjira-Tesama <wakjiratesama@gmail.com>
2026-06-28 22:32:00 +0300

    feat(landing): add new icon imports

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,5 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+  Search, Tractor, Store, Users, Leaf, ArrowRight,
+  Shield, ChevronRight, MapPin, Star, Wheat
