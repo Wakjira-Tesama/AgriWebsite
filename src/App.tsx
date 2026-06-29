84ef5994e7a7d0751176391a56e82f9b09dd23ae
Wakjira-Tesama <wakjiratesama@gmail.com>
2026-06-29 23:47:00 +0300

    feat(routes): add RegionalDashboard import

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
 import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
+
+// Admin Pages
+import AdminDashboard from './pages/Dashboard';
+import AdminLogin from './pages/Login';
